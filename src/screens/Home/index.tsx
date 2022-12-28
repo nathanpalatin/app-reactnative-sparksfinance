43e7585c8d07e05a36a6d9e3ef960192b39b8fad
Nathan Palatin <118611793+nathanpalatin@users.noreply.github.com>
2022-12-28 14:09:26 -0300

    Update index.tsx

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,39 @@
+import { useState } from 'react';
 import { styles } from './styles';
-import { View, Image, TextInput, TouchableOpacity, Text, Alert } from 'react-native';
+import { View, Image, TextInput, TouchableOpacity, Text, Alert, FlatList } from 'react-native';
+
 import { Task } from '../../components/Tasks';
 
 export function Home(){
 
-  function handleAddTask(){
-      Alert.alert("Todo", 'Ok, você adicionou uma nova tarefa.');
+  const [tasks, setTasks] = useState<String[]>([]);
+  const [taskName, setTaskName] = useState('')
+
+  function handleAddTask() {
+    if (tasks.includes(taskName)) {
+      return Alert.alert("Participante existe", "Já existe um participante na lista com esse nome.");
+    }
+
+    setTasks(prevState => [...prevState, taskName]);
+    setTaskName('');
+  }
+
+ 
+  function handleTaskRemove(name: string) {
+    Alert.alert("Remover", `Remover a tarefa ${name}?`, [
+      {
+        text: 'Sim',
+        onPress: () => setTaskName(prevState => prevState.filter(quest => quest !== name))
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ])
   }
 
   return (
+    
     <View style={styles.container} >
       <View style={styles.topBG}>
       <Image
@@ -22,7 +47,8 @@ export function Home(){
     style={styles.inputAdd}
     placeholder="Adicione uma nova tarefa"
     placeholderTextColor="#808080"
-
+    onChangeText={text => setTaskName(text)}
+    value={taskName}
     />
     <TouchableOpacity style={styles.btnAdd} onPress={handleAddTask}>
       <Text style={styles.textBtn}>
@@ -36,23 +62,39 @@ export function Home(){
       <Text style={styles.textTotalTasks}>Criadas 3</Text>
       <Text style={styles.textTasksDone}>Concluídas 0</Text>
     </View>
+   
     <View style={styles.myTasks}>
 
-    <Task />
-
-    <Image
-        style={styles.IMGnoTask}
-        source={require('../../images/notask.png')}
+    <FlatList
+      data={tasks}
+      keyExtractor={(item) => item}
+      renderItem={({ item }) => (
+      <Task 
+      key={Math.random()} 
+      task={item}
+      onRemove={() => handleTaskRemove(item)}
       />
+      
+      )} 
+      showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+         <>
+          <Image
+          style={styles.IMGnoTask}
+          source={require('../../images/notask.png')}
+          />
+
           <Text style={styles.noneTask}>
             <Text style={styles.noneTaskBold}>
               Você ainda não tem tarefas cadastradas
             </Text>
-          {"\n"}
-            Crie tarefas e organize seus itens a fazer</Text>
+              {"\n"}
+              Crie tarefas e organize seus itens a fazer
+          </Text>
+          </>
+          )}
+        />
+             </View>   
       </View>
-    
-  </View>
-  )
-
+    );
 }
